647346c54988635e91be537b7cd2dc83b2f5a013
Lunerio <lunerio@outlook.es>
2021-01-31 14:48:07 -0300

    repair betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,68 +2,68 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *i = NULL;
-    listint_t *j = (*list)->next;
-    listint_t *tmp = j;
+	listint_t *i = NULL;
+	listint_t *j = (*list)->next;
+	listint_t *tmp = j;
 
-    if (list == NULL || (*list)->next == NULL)
-	return;
+	if (list == NULL || (*list)->next == NULL)
+		return;
 
-    while (tmp)
-    {
-	i = tmp->prev;
-	while ((i->prev != NULL) && (i->prev->n > j->n))
+	while (tmp)
 	{
-	    i = i->prev;
-	}
-	if ((j->n) < (i->n))
-	{
-	    if (tmp->next == NULL) /*si movemos el ultimo*/
-	    {
-		j->prev->next = NULL;
-		if (i->prev == NULL) /*si tenemos que mover antes del primero*/
+		i = tmp->prev;
+		while ((i->prev != NULL) && (i->prev->n > j->n))
 		{
-		    i->prev = j;
-		    j->prev = NULL;
-		    j->next = i;
-		    *list = j;
-		    print_list(*list);
+			i = i->prev;
 		}
-		else /*si movemos al medio*/
+		if ((j->n) < (i->n))
 		{
-		    j->prev = i->prev;
-		    j->next = i;
-		    i->prev = j;
-		    j->prev->next = j;
-		    print_list(*list);
+			if (tmp->next == NULL)
+			{
+				j->prev->next = NULL;
+				if (i->prev == NULL)
+				{
+					i->prev = j;
+					j->prev = NULL;
+					j->next = i;
+					*list = j;
+					print_list(*list);
+				}
+				else
+				{
+					j->prev = i->prev;
+					j->next = i;
+					i->prev = j;
+					j->prev->next = j;
+					print_list(*list);
+				}
+			}
+			else
+			{
+				tmp = j->next;
+				if (i->prev == NULL)
+				{
+					j->prev->next = j->next;
+					j->next->prev = j->prev;
+					i->prev = j;
+					j->prev = NULL;
+					j->next = i;
+					*list = j;
+					print_list(*list);
+				}
+				else
+				{
+					j->prev->next = j->next;
+					j->next->prev = j->prev;
+					j->next = i;
+					j->prev = i->prev;
+					j->prev->next = j;
+					i->prev = j;
+					print_list(*list);
+				}
+			}
 		}
-	    }
-	    else /*si movemos algo del medio*/
-	    {
 		tmp = j->next;
-		if (i->prev == NULL) /*si tenemos que mover antes del primero*/
-		{
-		    j->prev->next = j->next;
-		    j->next->prev = j->prev;
-		    i->prev = j;
-		    j->prev = NULL;
-		    j->next = i;
-		    *list = j;
-		    print_list(*list);
-		}
-		else /*si movemos al medio*/
-		{
-		    j->prev->next = j->next;
-		    j->next->prev = j->prev;
-		    j->next = i;
-		    j->prev = i->prev;
-		    j->prev->next = j;
-		    i->prev = j;
-		    print_list(*list);
-		}
-	    }
+		j = tmp;
 	}
-	tmp = j->next;
-	j = tmp;
-    }
 }
