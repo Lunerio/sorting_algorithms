fb4a7e08e8322de8498658750034200ffd062530
Lunerio <lunerio@outlook.es>
2021-02-01 15:13:20 -0300

    save

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * quick_sort - function that sorts an array with lomuto scheme
+ * @array: array to sort
+ * @size: size of array
  */
 
 void quick_sort(int *array, size_t size)
@@ -11,41 +11,63 @@ void quick_sort(int *array, size_t size)
 	int lo = 0;
 	int hi = size - 1;
 
-	recursive(array, lo, hi);
+	recursive(array, size, lo, hi);
 }
 
-size_t partition(int *array, int lo, int hi)
+/**
+ * partition - arranges the array with a pivot in account
+ * @array: array to check
+ * @size: size of array
+ * @lo: lowest index
+ * @hi: highest index
+ * Return: i as the pivot
+ */
+
+int partition(int *array, size_t size, int lo, int hi)
 {
-	int pivot = array[hi];
+	int pivot = array[lo];
 	int save = 0;
 	int i = lo;
-	int j;
+	int j = hi;
 
-	for (j = lo; j < hi; j++)
+	while (i < j)
 	{
-		if (array[j] < pivot)
-		{
-			save = array[j];
-			array[j] = array[i];
-			array[i] = save;
+		while (array[i] <= pivot)
 			i++;
+		while (array[j] > pivot)
+			j--;
+		if (i < j)
+		{
+			save = array[i];
+			array[i] = array[j];
+			array[j] = save;
+			if (array[i] != array[j])
+				print_array(array, size);
 		}
-
 	}
-	save = array[i];
-	array[i] = array[hi];
-	array[hi] = save;
-	return i;
+	save = array[lo];
+	array[lo] = array[j];
+	array[j] = save;
+	if (array[j] != array[lo])
+		print_array(array, size);
+	return (j);
 }
+/**
+ * recursive - recursive function that arranges the sublists
+ * @array: array to arrange
+ * @size: size of array
+ * @lo: lowest index
+ * @hi: highest index
+ */
 
-void recursive(int *array, int lo, int hi)
+void recursive(int *array, size_t size, int lo, int hi)
 {
-	int i = 0;
+	int i;
 
 	if (lo < hi)
 	{
-		i = partition(array, lo, hi);
-		recursive(array, lo, i - 1);
-		recursive(array, i + 1, hi);
+		i = partition(array, size, lo, hi);
+		recursive(array, size, lo, i);
+		recursive(array, size, i + 1, hi);
 	}
 }
